a970888d7ef1ca70695d8a60c35f9e3ff6655966
Leonelfrias <162830492+Leonelfrias@users.noreply.github.com>
2024-11-06 18:15:23 +0100

    Initial commit

---
+++ assets/js/js.js
@@ -0,0 +1,24 @@
+const toggleMenu = () => {
+    const navigation = document.querySelector(".navigation");
+
+    const burgerMenu = document.querySelector(".burger-menu");
+    const src = burgerMenu.getAttribute("src");
+
+    const isBurger = src == "assets/burger-menu.svg";
+    const iconName = isBurger ? "assets/close.svg" : "assets/burger-menu.svg";
+
+    burgerMenu.setAttribute("src", iconName);
+
+    if (!isBurger) {
+        navigation.classList.add("navigation--mobile--fadeout");
+    setTimeout(() => {
+      navigation.classList.toggle("navigation--mobile");
+    }, 300);
+  } else {
+    navigation.classList.remove("navigation--mobile--fadeout");
+    navigation.classList.toggle("navigation--mobile");
+  }
+};
+
+
+
